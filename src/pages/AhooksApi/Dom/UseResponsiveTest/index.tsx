b7e13a33a8e9ca81b259566e37150f7bb5d2b64c
winfang <2536223474@qq.com>
2024-08-09 18:27:38 +0800

    8.9.2

--- src/pages/AhooksApi/Dom/UseResponsiveTest/index.tsx
+++ src/pages/AhooksApi/Dom/UseResponsiveTest/index.tsx
@@ -12,7 +12,7 @@ export default function UseResponsiveTest() {
 
   return (
     <>
-      <h3>获取响应式信息。</h3>
+      <h3>获取响应式信息。111</h3>
       <p>Please change the width of the browser window to see the effect: </p>
       {Object.keys(responsive).map((key) => (
         <p key={key}>
